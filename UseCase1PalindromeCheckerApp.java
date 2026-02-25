8f26d914f31ec6334314f501ae0b4e577a83228d
vs0737 <vs0737@srmist.edu.in>
2026-02-25 08:56:31 +0530

    Add UseCase1PalindromeCheckerApp.java for UC1 Application Entry & Welcome Message

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Application");
+        System.out.println("Version: 1.0");
+        System.out.println("Current Date and Time (UTC): 2026-02-25 03:26:23");
+    }
+}
