a75a5490518e61bb98e130921e700a3a98a121d4
vs0737 <vs0737@srmist.edu.in>
2026-02-25 09:14:54 +0530

    Create UseCase1PalindromeCheckerApp.java

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // Display Welcome Message
+        System.out.println("================================");
+        System.out.println("Palindrome Checker App");
+        System.out.println("================================");
+        System.out.println("Application Name: Palindrome Checker");
+        System.out.println("Application Version: 1.0");
+        System.out.println("================================");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("================================");
+    }
+}
